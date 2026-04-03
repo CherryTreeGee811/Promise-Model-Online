a3341e45435ec38d44f05684be1b4ffd65e01db8
SemiDoge <taelforlife@gmail.com>
2026-04-03 15:46:01 -0400

    Add testing proj for Api/Client

---
+++ PromiseModelOnline.Api.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace PromiseModelOnline.Api.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
