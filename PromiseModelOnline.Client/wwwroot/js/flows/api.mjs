80a23cffd9171f687450483a4825ad04e2da82df
SemiDoge <taelforlife@gmail.com>
2026-05-16 21:34:41 -0400

    Add functions to add projects+promises stack

--- PromiseModelOnline.Client/wwwroot/js/flows/api.mjs
+++ PromiseModelOnline.Client/wwwroot/js/flows/api.mjs
@@ -49,3 +49,29 @@ export function getMomentsByFlow(flowId) {
         }
     });
 }
+
+export async function addFlow(flow) {
+    const url = `${base}/api/flows`;
+    const token = getAccessTokenFromCookie();
+
+    const res = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Language': 'en-CA',
+        },
+        body: JSON.stringify(flow)
+    });
+
+    if (res.ok) {
+        if (res.status === 204) return null;
+        return res.json();
+    } else if (res.status === 401) {
+        document.getElementById("login-link")?.click();
+    } else {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    }
+}
--- PromiseModelOnline.Client/wwwroot/js/promises/api.mjs
+++ PromiseModelOnline.Client/wwwroot/js/promises/api.mjs
@@ -49,3 +49,29 @@ export function getEpicsByPromise(promiseId) {
         }
     });
 }
+
+export async function addPromise(promise) {
+    const url = `${base}/api/promises`;
+    const token = getAccessTokenFromCookie();
+
+    const res = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Language': 'en-CA',
+        },
+        body: JSON.stringify(promise)
+    });
+
+    if (res.ok) {
+        if (res.status === 204) return null;
+        return res.json();
+    } else if (res.status === 401) {
+        document.getElementById("login-link")?.click();
+    } else {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    }
+}
