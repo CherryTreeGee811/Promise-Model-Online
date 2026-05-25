afe28c2a672c582a34dcb68a64a807e41dd33e7b
SemiDoge <taelforlife@gmail.com>
2026-05-25 02:46:51 -0400

    Commented out stale test

--- PromiseModelOnline.Client.Tests/Tests/ProjectsListTests.cs
+++ PromiseModelOnline.Client.Tests/Tests/ProjectsListTests.cs
@@ -18,30 +18,31 @@ namespace PromiseModelOnline.Client.Tests.Tests
             Assert.That(row.Text, Does.Contain("Test Project"));
         }
 
-        [Test]
-        public void ProjectList_DeleteButton_RemovesRow()
-        {
-            EnsureLoggedIn();
-            NavigateSpa("/projects");
+        // TODO: This test will need to be rewritten/moved to a Project Settings UI Tests suite at a later date
+        // [Test]
+        // public void ProjectList_DeleteButton_RemovesRow()
+        // {
+        //     EnsureLoggedIn();
+        //     NavigateSpa("/projects");
 
-            var deleteBtn = WaitForElement(By.CssSelector(".delete-btn[project-id='1']"));
-            deleteBtn.Click();
+        //     var deleteBtn = WaitForElement(By.CssSelector(".delete-btn[project-id='1']"));
+        //     deleteBtn.Click();
 
-            // After deletion, the row should be removed from the DOM
-            WaitUntil(driver =>
-            {
-                try
-                {
-                    return driver.FindElements(By.CssSelector("#project-list-table-body tr")).Count == 0;
-                }
-                catch
-                {
-                    return false;
-                }
-            }, 5);
+        //     // After deletion, the row should be removed from the DOM
+        //     WaitUntil(driver =>
+        //     {
+        //         try
+        //         {
+        //             return driver.FindElements(By.CssSelector("#project-list-table-body tr")).Count == 0;
+        //         }
+        //         catch
+        //         {
+        //             return false;
+        //         }
+        //     }, 5);
 
-            var rows = Driver.FindElements(By.CssSelector("#project-list-table-body tr"));
-            Assert.That(rows.Count, Is.EqualTo(0), "Project row was not removed after deletion");
-        }
+        //     var rows = Driver.FindElements(By.CssSelector("#project-list-table-body tr"));
+        //     Assert.That(rows.Count, Is.EqualTo(0), "Project row was not removed after deletion");
+        // }
     }
 }
