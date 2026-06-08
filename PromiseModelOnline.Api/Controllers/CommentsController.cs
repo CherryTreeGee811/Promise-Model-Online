8f243d5aaec0d2809c3be5dd855b7f50cfbf8c05
SemiDoge <taelforlife@gmail.com>
2026-06-07 20:44:15 -0400

    Fix API tests

--- PromiseModelOnline.Api/Controllers/CommentsController.cs
+++ PromiseModelOnline.Api/Controllers/CommentsController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using PromiseModelOnline.Api.BusinessLogic.Interfaces;
 using PromiseModelOnline.Api.DAL.Interfaces;
 using PromiseModelOnline.Api.DTOs;
+using PromiseModelOnline.Api.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
 
@@ -16,12 +18,15 @@ namespace PromiseModelOnline.Api.Controllers
     {
         private readonly ICommentService _commentService;
         private readonly IUserRepository _userRepository;
+        private readonly ICommentRepository _commentRepository;
 
         public CommentsController(ICommentService commentService,
-                                  IUserRepository userRepository)
+                                  IUserRepository userRepository,
+                                  ICommentRepository commentRepository)
         {
             _commentService = commentService;
             _userRepository = userRepository;
+            _commentRepository = commentRepository;
         }
 
         [Authorize(Policy = "projects.read")]
@@ -61,6 +66,50 @@ namespace PromiseModelOnline.Api.Controllers
                     new { type = dto.ParentType, parentId = dto.ParentId }, comment);
             }
             catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(Policy = "projects.read")]
+        [HttpGet("search-users")]
+        public async Task<ActionResult<IEnumerable<User>>> SearchUsers(
+            [FromQuery] string? type,
+            [FromQuery] int parentId,
+            [FromQuery] string searchTerm = "")
+        {
+            if (string.IsNullOrEmpty(type) || parentId <= 0 || string.IsNullOrEmpty(searchTerm))
+                return Ok(Enumerable.Empty<User>());
+
+            try
+            {
+                var projectId = await _commentRepository.ResolveProjectIdAsync(type, parentId);
+                var users = await _userRepository.SearchUsersByProjectAsync(projectId, searchTerm);
+                return Ok(users);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(Policy = "projects.read")]
+        [HttpGet("search-promises")]
+        public async Task<ActionResult<IEnumerable<StackSearchResult>>> SearchPromises(
+            [FromQuery] string? type,
+            [FromQuery] int parentId,
+            [FromQuery] string searchTerm = "")
+        {
+            if (string.IsNullOrEmpty(type) || parentId <= 0 || string.IsNullOrEmpty(searchTerm))
+                return Ok(Enumerable.Empty<StackSearchResult>());
+
+            try
+            {
+                var projectId = await _commentRepository.ResolveProjectIdAsync(type, parentId);
+                var results = await _commentRepository.SearchStackByStatementAsync(projectId, searchTerm);
+                return Ok(results);
+            }
+            catch (ArgumentException ex)
             {
                 return BadRequest(ex.Message);
             }
