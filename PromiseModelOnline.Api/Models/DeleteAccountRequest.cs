6cc1c83cd98e9b788c66c7aacd4f80fa71efc77b
SemiDoge <taelforlife@gmail.com>
2026-06-12 16:17:40 -0400

    Try fix Merge 2

--- PromiseModelOnline.Api/Models/DeleteAccountRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace PromiseModelOnline.Api.Models;
-
-public class DeleteAccountRequest
-{
-    [JsonPropertyName("password")]
-    public string Password { get; set; } = string.Empty;
-}
--- PromiseModelOnline.Api/Models/LogoutRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace PromiseModelOnline.Api.Models;
-
-public class LogoutRequest
-{
-    [JsonPropertyName("refreshToken")]
-    public string? RefreshToken { get; set; }
-}
